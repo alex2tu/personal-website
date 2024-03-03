1efb8faae74f650e2fb61f95dee4a20151a3189a
alex2tu <alexandertu0215@gmail.com>
2024-03-03 01:12:34 -0500

    improved typing animation

--- js/intro.js
+++ js/intro.js
@@ -2,28 +2,32 @@ let introRunning = true;
 
 let target = document.getElementById("header-title");
 let anim = [
-    { word: "_", ms: 200 },
+    { word: "|", ms: 200 },
     { word: " ", ms: 200 },
-    { word: "_", ms: 200 },
+    { word: "|", ms: 200 },
     { word: " ", ms: 200 },
-    { word: "H", ms: 150 },
-    { word: "Hi", ms: 150 },
-    { word: "Hi,", ms: 150 },
-    { word: "Hi, I", ms: 150 },
-    { word: "Hi, I'", ms: 150 },
-    { word: "Hi, I'm", ms: 150 },
-    { word: "Hi, I'm ", ms: 100 },
-    { word: "Hi, I'm A", ms: 150 },
-    { word: "Hi, I'm Al", ms: 150 },
-    { word: "Hi, I'm Ale", ms: 150 },
-    { word: "Hi, I'm Alex", ms: 150 },
-    { word: "Hi, I'm Alex ", ms: 200 },
-    { word: "Hi, I'm Alex 2", ms: 200 },
+    { word: "H|", ms: 150 },
+    { word: "Hi|", ms: 120 },
+    { word: "Hi,|", ms: 150 },
+    { word: "Hi, |", ms: 100 },
+    { word: "Hi, I|", ms: 120 },
+    { word: "Hi, I'|", ms: 150 },
+    { word: "Hi, I'm|", ms: 150 },
+    { word: "Hi, I'm |", ms: 100 },
+    { word: "Hi, I'm A|", ms: 150 },
+    { word: "Hi, I'm Al|", ms: 120 },
+    { word: "Hi, I'm Ale|", ms: 150 },
+    { word: "Hi, I'm Alex|", ms: 120 },
+    { word: "Hi, I'm Alex |", ms: 200 },
+    { word: "Hi, I'm Alex 2|", ms: 400 },
     { word: "Hi, I'm Alex 2", ms: 300 },
-    { word: "Hi, I'm Alex 2", ms: 400 },
-    { word: "Hi, I'm Alex ", ms: 200 },
-    { word: "Hi, I'm Alex T", ms: 200 },
-    { word: "Hi, I'm Alex Tu", ms: 800 },
+    { word: "Hi, I'm Alex 2|", ms: 400 },
+    { word: "Hi, I'm Alex |", ms: 200 },
+    { word: "Hi, I'm Alex T|", ms: 200 },
+    { word: "Hi, I'm Alex Tu|", ms: 400 },
+    { word: "Hi, I'm Alex Tu", ms: 300 },
+    { word: "Hi, I'm Alex Tu|", ms: 400 },
+    { word: "Hi, I'm Alex Tu", ms: 100 },
     { word: "Hi, I'm Alex Tu", ms: 200 }
 ];
 let i = 0;
@@ -43,7 +47,7 @@ let update = () => {
                 backgroundColor: currTileColor,
                 delay: anime.stagger(75, {
                   grid: [columns, rows],
-                  from: 'center'
+                  from: 'last'
                 }), 
                 complete: function() {
                   console.log("Animation completed!");
