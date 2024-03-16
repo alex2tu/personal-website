d093a5c5394d7318a6a422fc0f26691c6aedafc1
alex2tu <alexandertu0215@gmail.com>
2024-03-15 21:58:39 -0400

    updated intro

--- js/intro.js
+++ js/intro.js
@@ -1,6 +1,6 @@
 let introRunning = true;
 
-let target = document.getElementById("header-title");
+let header = document.getElementById("header-title");
 let anim = [
     { word: "|", ms: 400 },
     { word: " ", ms: 300 },
@@ -10,26 +10,67 @@ let anim = [
     { word: "Al|", ms: 100 },
     { word: "Ale|", ms: 100 },
     { word: "Alex|", ms: 100 },
-    { word: "Alex |", ms: 150 },
-    { word: "Alex 2|", ms: 400 },
-    { word: "Alex 2", ms: 300 },
-    { word: "Alex 2|", ms: 400 },
-    { word: "Alex 2", ms: 300 },
+    { word: "Alex |", ms: 200 },
+    { word: "Placeholder", ms: 0 }, //index to skip
     { word: "Alex |", ms: 200 },
     { word: "Alex T|", ms: 200 },
     { word: "Alex Tu|", ms: 400 },
     { word: "Alex Tu", ms: 300 },
     { word: "Alex Tu|", ms: 400 },
     { word: "Alex Tu", ms: 200 },
-    { word: "Alex Tu", ms: 200 }
+    { word: "Alex Tu", ms: 250 }
+];
+
+const names = [
+  [
+    { word: "Alex T|", ms: 200 },
+    { word: "Alex To|", ms: 200 },
+    { word: "Alex Too|", ms: 400 },
+    { word: "Alex Too", ms: 300 },
+    { word: "Alex Too|", ms: 400 },
+    { word: "Alex Too", ms: 200 },
+    { word: "Alex To|", ms: 200 },
+    { word: "Alex T|", ms: 200 },
+    { word: "Alex T|", ms: 200 }
+  ],
+  [
+    { word: "Alex T|", ms: 200 },
+    { word: "Alex To|", ms: 400 },
+    { word: "Alex To", ms: 300 },
+    { word: "Alex To|", ms: 400 },
+    { word: "Alex To", ms: 300 },
+    { word: "Alex T|", ms: 200 },
+    { word: "Alex T|", ms: 200 }
+  ],
+  [
+    { word: "Alex 2|", ms: 400 },
+    { word: "Alex 2", ms: 300 },
+    { word: "Alex 2|", ms: 400 },
+    { word: "Alex 2", ms: 300 },
+    { word: "Alex 2", ms: 300 }
+  ],
+  [
+    { word: "Alex T|", ms: 200 },
+    { word: "Alex Tw|", ms: 200 },
+    { word: "Alex Two|", ms: 400 },
+    { word: "Alex Two", ms: 300 },
+    { word: "Alex Two|", ms: 400 },
+    { word: "Alex Two", ms: 300 },
+    { word: "Alex Tw|", ms: 200 },
+    { word: "Alex T|", ms: 200 },
+    { word: "Alex T|", ms: 200 }
+  ]
 ];
 let i = 0;
 let update = () => {
     let step = anim[i];
-    target.innerText = step.word;
+    header.innerText = step.word;
     i++;
 
-    if (i < anim.length) {
+    if (i == 9) {
+      console.log("1");
+      setTimeout(update_last_names, 200);
+    } else if (i < anim.length) {
         setTimeout(update, step.ms);
     } else {
         console.log("started")
@@ -50,4 +91,30 @@ let update = () => {
         }, 500);
     }
 };
+
+function choose_name() {
+  let index = Math.floor(Math.random() * 4);
+  console.log(index);
+  console.log(names[index]);
+  return names[index];
+}
+
+let j = 0;
+let chosen_name = choose_name();
+console.log(chosen_name);
+let update_last_names = () => {
+  console.log("2");
+  let step = chosen_name[j];
+  header.innerText = step.word;
+  j++;
+
+  if (j < chosen_name.length) {
+    setTimeout(update_last_names, step.ms)
+  } else {
+    i++;
+    update();
+  }
+
+};
+
 update();
